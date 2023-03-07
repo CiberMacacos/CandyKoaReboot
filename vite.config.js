96a67e312ea041cd69da983c35bb3f059986507a
Manuel Rodríguez Santana <lolo@turitop.com>
2023-03-07 19:55:59 +0000

    add log

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ const isGitHubPages = false;
 const folderName = path.basename(process.cwd()) + "/";
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
-console.log("Deberiamos estar en una rama");
-if(process.env.NODE_ENV === "development") {
-  console.log("Estamos en modo desarrollador");
-} else {
+console.log("Deberiamos estar en una rama!");
+
   console.log("Estamos en modo " + process.env.NODE_ENV);
-}
+
 
 export default {
   root: "src",
