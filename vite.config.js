20d0698723e530a2acdb1e31c09f662e580a482b
Manuel Rodríguez Santana <lolo@turitop.com>
2023-03-07 19:50:45 +0000

    Add branch deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const isGitHubPages = false;
 const folderName = path.basename(process.cwd()) + "/";
 const mode = process.env.NODE_ENV === "production" ? "production" : "development";
 const base = mode === "production" && isGitHubPages ? "/" + folderName : "/";
-console.log("Deberiamos estar en un pull request");
+console.log("Deberiamos estar en una rama");
 if(process.env.NODE_ENV === "development") {
   console.log("Estamos en modo desarrollador");
 } else {
